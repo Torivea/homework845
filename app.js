ba243bd53a951c04efafec3efa92ee8d28327b15
Tori Vea <tori.vea@hotmail.com>
2024-04-15 20:08:59 +0200

    test1

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-
 var app = express();
 
 // view engine setup
@@ -18,7 +16,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// New route for the main page endpoint "/"
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,14 +36,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-
-// Define the main page endpoint
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World' });
-});
-
-
-
 
 module.exports = app;
